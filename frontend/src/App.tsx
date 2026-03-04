9e36ea1a80709cea6184dc1246e39ec715539d9a
Kunal Singh <kunalindia59@gmail.com>
2026-03-04 16:36:54 +0530

    Adding more routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,9 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Signin from './components/Signin'
 import DashBoard from './pages/Dashboard'
+import SalesBill from './components/SalesBill'
+import Temp from './pages/Temp'
+
 
 function App() {
 
@@ -12,6 +15,8 @@ function App() {
       <Routes>
         <Route path='/signin' element={<Signin />} />
         <Route path='/' element={<DashBoard />} />
+        <Route path='/bill' element={<SalesBill />} />
+        <Route path='/temp' element={<Temp />} />
       </Routes>
     </>
   )
