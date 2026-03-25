081fc0b30cea5e45e22145ea3fda253a4079de5d
Kunal Singh <kunalindia59@gmail.com>
2026-03-25 23:23:33 +0530

    Adding pages

---
+++ frontend/src/pages/Dashboard.tsx
@@ -0,0 +1,10 @@
+import type React from "react"
+import DashBoard from "../components/dashboard/DashBoard"
+
+const Dashboard: React.FC = () => {
+    return (
+        <DashBoard />
+    )
+}
+
+export default Dashboard
